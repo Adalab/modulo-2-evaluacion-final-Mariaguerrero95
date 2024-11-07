f51f0395a8b4ebc6806175f41acf07d44996057f
Mariaguerrero95 <mariaguerrero1995@outlook.es>
2024-11-07 17:29:50 +0100

    changes in js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
 "use strict";
 /*
-    PEQUEÑOS PASOS
-    PRIMERA PARTE (BUSCAR)
+    PASOS EN HUMANO:
+
+    PRIMERA PARTE, BUSCAR
     - Pintar los elementos de mi HTML 
     - Cuando la usuaria haga click en Botón Buscar que me aparezca la lista de series (if)
     - Seleccionar elementos globales para el anterior paso (para guardarlos en variables y constantes):
@@ -11,6 +12,19 @@
         - Lista de favoritos
     - Recoger los datos de la elección de la usuaria (en caso que haya escrito y pinchado en buscar)
     - Que se muestren en el DOM lo que ha buscado la usuaria
+
+    SEGUNDA PARTE, FAVORITOS
+    - Se cambiaran el color de fondo  y de fuente de la serie seleccionada. se añadirá a favoritos
+    - Mostrar la elección favorita de la usuaria en la parte izquierda de la pantalla debajo del formulario de búsqueda.
+    - La serie favorita seguirá mostrándose en el DOM aunque la usuaria realice otra búsqueda
+
+    TERCERA PARTE
+    - Guardar las selecciones favoritas de la usuaria en el navegador local.
+    - Al recargar la pagina se mostrará el listado de favoritos
+
+    CUARTA PARTE (BONUS)
+    - Cuando un fav ya esté añadido, se cambiará su background para que se vea resaltado. Si haces clic nuevamente en una serie que ya es favorita, deberíamos quitarla de favoritos
+
 */
 
 //constantes globales
@@ -19,25 +33,33 @@ const searchButton = document.querySelector(".js-search");
 const resultsSection = document.querySelector(".js-container");
 const favoriteSeries = document.querySelector(".js-favorites");
 
-//Creo también una variable global en forma de array de las series
+//variable global en forma de array de las series (let porque variará)
 let resultsList = [];
 let favoriteSeriesList = [];
 
-//creo una variable global de las series
+//Variable global de las series
 const getFavorite = localStorage.getItem("favorites")
 
 if(getFavorite !== null){
-    resultsList = JSON.parse(getFavorite); // Parsea los datos
+    favoriteSeriesList = JSON.parse(getFavorite); // Parsea los datos
+    //console.log("Favoritos recuperados desde localStorage:", favoriteSeriesList);
 }
 
 //Defino una función para renderizar series, aquí utilizaré document.createElement para crear nuevos elementos en el DOM, cada vez que se llama a esta función para generar nuevos elementos HTML  para mostrar cada serie en el DOM
 function renderingSeries (series, resultsSection){
     resultsSection.innerHTML =""; //Pongo comillas vacías para que vacie la sección de resultados
-    //A continuación pintaré las series en la página
+    // A continuación pintaré las series en la página
     for (const serie of series){
         const listOfSeries = document.createElement ("div");
          // Con el id(mal_id lo he conseguido del enlace de la API) con el mal_id identifico cuando la usuaria ha hecho click
         listOfSeries.id = serie.mal_id;
+
+        //BONUS, verificar su la serie es favorita y aplicarle el estilo
+        const itsFavorite = favoriteSeriesList.some(fav => fav.mal_id === serie.mal_id);
+        if (itsFavorite) {
+            listOfSeries.style.backgroundColor = "#f3fbe1"; // Color fondo la serie favorita
+            listOfSeries.style.color = "rgb(66, 236, 227)"; // Color de texto serie favorita
+        }
 
         // Ahora, con el DOM crearé los elementos y subelementos de la lista de series, ya que he dejado vaciado el HTML
         const title = document.createElement("h2");
@@ -45,9 +67,9 @@ function renderingSeries (series, resultsSection){
         title.appendChild(textTitle);
 
         const cardImg = document.createElement("img");
-        //cardImg.setAttribute("src", serie.images.jpg.image_url);
-        // Verificar si la imagen de la serie es la imagen predeterminada de MyAnimeList
         const imageUrl = serie.images.jpg.image_url;
+
+        //Ahora, si una serie no tiene imagen le pondré un estilo determinado y si no... (if, else)
         if (imageUrl === "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png") {
             // Si la imagen es la predeterminada, usar una imagen de placeholder
             cardImg.setAttribute("src", "https://via.placeholder.com/210x295/ffcc00/666666/?text=Anime");
@@ -71,8 +93,8 @@ function renderingSeries (series, resultsSection){
         listOfSeries.addEventListener("click",handleFavoriteSeries);
     }
 }
-//Para esucchar el click de la usuaria en la serie dependiendo del id que escriba creo una función 
 
+// Función para manejar el click en una serie para agregarla a favoritos
 function handleFavoriteSeries (event){
     const idClick = (event.currentTarget.id);
    //console.log("handleFavoriteSeries ejecutada") SE EJECUTA BIEN
@@ -92,10 +114,13 @@ function handleFavoriteSeries (event){
     //Ahora voy a verificar si una serie seleccionada (if) 
     if (indexSerieFavorite === -1){
         favoriteSeriesList.push(idSerieSelected); //Añade la serie
-        localStorage.setItem("favoritos", JSON.stringify(favoriteSeriesList)); //Guarda como JSON string
+        localStorage.setItem("favoritos", JSON.stringify(favoriteSeriesList));
+        console.log("Favoritos actualizados", favoriteSeriesList) 
+        //Guarda como JSON string
         renderingSeries(favoriteSeriesList, favoriteSeries);
     }
 };
+
 
 //Creo una función flecha donde meteré el fetch (API)
 const getApiSeries = () => {
