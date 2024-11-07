ee7868c5aefa6b7e693df87ce61a76bacd403eba
Mariaguerrero95 <mariaguerrero1995@outlook.es>
2024-11-07 09:47:49 +0100

    starting jsvascript

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
 "use strict";
-
-
+/*
+    PEQUEÑOS PASOS
+    - Pintar los elementos de mi HTML 
+    - Seleccionar elementos globales (constantes globales)
+    - Pido los datos al servidor
+    - Acceder/guardar datos de series favoritas
+*/
+//constantes globales
